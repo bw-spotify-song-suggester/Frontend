b318383404993e8f94cf5c5208bae957ea7e6019
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-06 01:30:19 -0500

    still working on home page, trying to fetch data for passing into props

--- spotify-app/src/components/HomePage.js
+++ spotify-app/src/components/HomePage.js
@@ -4,76 +4,74 @@ import styled from 'styled-components'
 import { axiosWithAuth } from '../utilities/axiosWithAuth';
 import MaterialTable from 'material-table';
 
-export default function HomePage(props) {
-  const [state, setState] = useState({
-    columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-      {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-      },
-    ],
-    data: [
-      { name: ``, surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
-      },
-    ],
-  });
-console.log(props)
-  return (
-    <MaterialTable
-      title="Editable Example"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-    />
+// export default function HomePage(props) {
+//   const [state, setState] = useState({
+//     columns: [
+//       { title: 'Name', field: 'name' },
+//       { title: 'Surname', field: 'surname' },
+//       { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+//       {
+//         title: 'Birth Place',
+//         field: 'birthCity',
+//         lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+//       },
+//     ],
+//     data: [
+//       { name: ``, surname: 'Baran', birthYear: 1987, birthCity: 63 },
+//       {
+//         name: 'Zerya Betül',
+//         surname: 'Baran',
+//         birthYear: 2017,
+//         birthCity: 34,
+//       },
+//     ],
+//   });
+// console.log(props)
+//   return (
+//     <MaterialTable
+//       title="Editable Example"
+//       columns={state.columns}
+//       data={state.data}
+//       editable={{
+//         onRowAdd: newData =>
+//           new Promise(resolve => {
+//             setTimeout(() => {
+//               resolve();
+//               setState(prevState => {
+//                 const data = [...prevState.data];
+//                 data.push(newData);
+//                 return { ...prevState, data };
+//               });
+//             }, 600);
+//           }),
+//         onRowUpdate: (newData, oldData) =>
+//           new Promise(resolve => {
+//             setTimeout(() => {
+//               resolve();
+//               if (oldData) {
+//                 setState(prevState => {
+//                   const data = [...prevState.data];
+//                   data[data.indexOf(oldData)] = newData;
+//                   return { ...prevState, data };
+//                 });
+//               }
+//             }, 600);
+//           }),
+//         onRowDelete: oldData =>
+//           new Promise(resolve => {
+//             setTimeout(() => {
+//               resolve();
+//               setState(prevState => {
+//                 const data = [...prevState.data];
+//                 data.splice(data.indexOf(oldData), 1);
+//                 return { ...prevState, data };
+//               });
+//             }, 600);
+//           }),
+//       }}
+//     />
 
-import React, { useEffect, useState }from 'react';
-import styled from 'styled-components';
-import { axiosWithAuth } from '../utilities/axiosWithAuth';
+
 
 const Div = styled.div`
 text-align:center;
@@ -110,4 +108,6 @@ const HomePage = () => {
 
   );
 }
+
+export default HomePage;
  
