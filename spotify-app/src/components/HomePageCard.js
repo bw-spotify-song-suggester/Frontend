caa821e4a5b8cef5c751c95b7f55cd0813f83723
Kris Monroe <krisdmonroe@gmail.com>
2020-02-06 08:08:26 -0800

    Added basic styling to cards from search

--- spotify-app/src/components/HomePageCard.js
+++ spotify-app/src/components/HomePageCard.js
@@ -1,20 +1,85 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState } from "react";
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 import styled from 'styled-components'
-import { axiosWithAuth } from '../utilities/axiosWithAuth';
-import MaterialTable from 'material-table';
-import  { SongContext } from '../contexts/SongContext'
 
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+}));
 
+
+const Div = styled.div`
+margin:2% auto;
+max-width:30%;
+
+`;
 const HomePageCard = (props) => {
+const classes = useStyles();
+const theme = useTheme();
     console.log('this is props',props)
 
 
     return (
-        <div key={props.key}>
-        <p> Song Name: {props.song} </p>
-        <p> Artist: {props.artist} </p>
-         </div>
+        <Div>
+        <Card className={classes.root}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5" key={props.key}>
+          {props.song}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+          {props.artist}
+          </Typography>
+        </CardContent>
+        <div className={classes.controls}>
+          <IconButton aria-label="previous">
+            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+          </IconButton>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon className={classes.playIcon} />
+          </IconButton>
+          <IconButton aria-label="next">
+            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+          </IconButton>
+        </div>
+      </div>
+      <CardMedia
+        className={classes.cover}
+        image="/static/images/cards/live-from-space.jpg"
+        title="Live from space album cover"
+      />
+    </Card>
+    </Div>
     )
 }
 
